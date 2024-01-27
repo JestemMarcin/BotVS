668b9f8c750eb03e9e87195a8d81dfc29a93216f
O120D6 <o120d6@gmail.com>
2024-01-27 23:58:29 +0100

    firstCommit

--- bot.py
+++ bot.py
@@ -1,72 +1,67 @@
-# bot.py
 import os
-from discord.utils import get
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 from BossTimer import BossTimer
-import time
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-CHANNEL_MAIN = False
-MESSAGE_MAIN = False
-bot = commands.Bot(command_prefix = '!',intents=discord.Intents.all())
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 boss_timer = BossTimer()
 
 @bot.event
+async def on_ready():
+    print('Bot jest gotowy!')
+
+@bot.event
 async def on_message(message):
-    if CHANNEL_MAIN != False:
-    if message.channel == CHANNEL_MAIN:
-    
-        if not message.author.bot and not message.content.startswith('!'):
-            await message.delete()
+    if not message.author.bot and not message.content.startswith('!'):
+        await message.delete()
 
     await bot.process_commands(message)
 
-@bot.command(name='chimera')
-async def resetTime(ctx):
-    print(ctx)
-    response = "s"
-    await ctx.send(response)
-
 @bot.command(name='init')
-async def initBot(ctx):
-    global CHANNEL_MAIN
-    guild = ctx.message.guild
-    for channel in guild.channels:
-        print(channel.name)
-        if channel.name == 'bosstimer-by-o120d6': 
-            CHANNEL_MAIN = channel
-    if CHANNEL_MAIN == False:
-        CHANNEL_MAIN = await guild.create_text_channel('bosstimer-by-o120d6')
-        await CHANNEL_MAIN.send("inicjalizacja pomyślna "+CHANNEL_MAIN.name)
+async def init_bot(ctx):
+    channel_name = 'bosstimer-by-o120d6'
+    channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+    if not channel:
+        channel = await ctx.guild.create_text_channel(channel_name)
+        await channel.send(f"Inicjalizacja pomyślna na kanale {channel_name}")
     else:
-        await CHANNEL_MAIN.send("inicjalizacja pomyślna "+CHANNEL_MAIN.name)
+        await channel.send(f"Inicjalizacja pomyślna na kanale {channel_name}")
 
-@bot.command(name='reset', help='Use it like that "reset chimera"')
-async def resetBoss(ctx, boss_command: str):
+@bot.command(name='reset', help='Użycie: !reset chimera')
+async def reset_boss(ctx, boss_command: str):
     boss_timer.resetTime(boss_command)
 
-@bot.command(name='addBoss', help='Use it like that "add Bigchimera chimera timetoresetinminutes"')
-async def addBoss(ctx, boss_name: str, boss_command: str, boss_time: int):
-    
+@bot.command(name='addBoss', help='Użycie: !addBoss Bigchimera chimera timetoresetinminutes')
+async def add_boss(ctx, boss_name: str, boss_command: str, boss_time: int):
     boss_timer.addBoss(boss_name, boss_command, boss_time)
 
-@bot.command(name="display")
-async def displayBossTime(ctx):
-    global CHANNEL_MAIN
-    global MESSAGE_MAIN
-    if MESSAGE_MAIN == False:
-        async for message in CHANNEL_MAIN.history(limit=1):
-            MESSAGE_MAIN = message
-            break
+@bot.command(name='display')
+async def display_boss_time(ctx):
+    channel_name = 'bosstimer-by-o120d6'
+    channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+    if channel:
+        async for message in channel.history(limit=1):
+            try:
+                await message.edit(content=boss_timer.textWall())
+            except discord.errors.Forbidden:
+                print(message.content)
+                await ctx.send("Nie mam uprawnień do edycji wiadomości.")
+    else:
+        await ctx.send(f"Nie znaleziono kanału o nazwie {channel_name}.")
 
-    await MESSAGE_MAIN.edit(content = boss_timer.textWall())
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Nieznana komenda!")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Brakujący argument!")
+    else:
+        await ctx.send(f"Wystąpił błąd: {error}")
 
-# Cog error handler
-async def cog_command_error(self, ctx, error):
-    await ctx.send(f"An error occurred in the Test cog: {error}")
 
 
 bot.run(TOKEN)
