6495e5233a1734c45fce597d1662ffc87040f137
O120D6 <o120d6@gmail.com>
2024-01-29 03:19:08 +0100

    idk, got mess of the code since making discord bot first time and with chaoticgpt,but should work on one guild

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from BossTimer import BossTimer
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 channel = 0
+guild = 0
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 boss_timer = BossTimer()
 
@@ -15,30 +16,33 @@ async def on_ready():
     print('Bot jest gotowy!')
 
 @bot.event
-async def on_message(message):
-    if message.channel.name == 'bosstimer-by-o120d6' and message.author != bot.user:
-        await message.delete()
-    elif message.channel.name == 'world-boss-spawn-log':
-        boss_command = message.content.replace(" ", "")  # Usunięcie spacji
+async def on_message(ctx):
+    global channel
+    global guild
+    if ctx.channel == channel and ctx.author != bot.user:
+        await ctx.delete()
+    elif ctx.guild == guild and ctx.channel.name == 'world-boss-spawn-log':
+        boss_command = ctx.content.replace(" ", "").lower()  # Usunięcie spacji
         for boss in boss_timer.bosses:
             if boss.command in boss_command:
                 boss_timer.resetTime(boss_command)
-                await display_boss_time(message.channel)
+                await display_boss_time(ctx)
                 break
     else:
-        await bot.process_commands(message)
+        await bot.process_commands(ctx)
 
 @bot.command(name='init')
 async def init_bot(ctx):
-    channel_name = 'bosstimer-by-o120d6'
     global channel
-    channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+    global guild
+    channel = discord.utils.get(ctx.guild.channels, name='bosstimer-by-o120d6')
+    guild = ctx.guild
     if channel:
         await clear_channel(channel)
     else:
-        channel = await ctx.guild.create_text_channel(channel_name)
-        await channel.send(f"Inicjalizacja pomyślna na kanale {channel_name}")
+        channel = await ctx.guild.create_text_channel('bosstimer-by-o120d6')
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)  # Ustawienie uprawnień
+    await channel.send(f"Inicjalizacja pomyślna na kanale {channel.name}")
 
 async def clear_channel(channel):
     async for message in channel.history():
@@ -50,7 +54,7 @@ async def reset_boss(ctx, boss_command: str):
         if boss.command == boss_command:
             boss_timer.resetTime(boss_command)
             await ctx.send(f"Czas bossa {boss.name} został zresetowany.")
-            await display_boss_time(ctx.channel)
+            await display_boss_time()
 
 @bot.command(name='resetall', help='Użycie: !resetall')
 async def resetall_boss(ctx):
@@ -61,11 +65,10 @@ async def resetall_boss(ctx):
 @bot.command(name='addBoss', help='Użycie: !addBoss Bigchimera chimera timetoresetinminutes')
 async def add_boss(ctx, boss_name: str, boss_command: str, boss_time: int):
     boss_timer.addBoss(boss_name, boss_command, boss_time)
-    await ctx.send(f"Dodano nowego bossa: {boss_name}")
+    await ctx.send(f"Dodano nowego bossa: {boss_name.lower()}")
 
 @bot.command(name='display')
-async def display_boss_time(channel):
-    channel_name = 'bosstimer-by-o120d6'
+async def display_boss_time(ctx):
     global channel
     if channel:
         async for message in channel.history(limit=1):
@@ -75,7 +78,7 @@ async def display_boss_time(channel):
                 pass
         await channel.send(boss_timer.textWall())
     else:
-        print(f"Nie znaleziono kanału o nazwie {channel_name}.")
+        print(f"Nie znaleziono kanału.")
 
 @bot.event
 async def on_command_error(ctx, error):
